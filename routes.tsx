44b216707780dd7f6e3eb9994d408ca72fa992e1
Felipe alexandre <lipe.alx@gmail.com>
2025-04-13 12:13:36 -0300

    Fix: some problems

--- routes.tsx
+++ routes.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom"
-import Perfil from './src/Pages/Perfil'
-import Home from "./src/Pages/Home"
+//import Perfil from './src/Pages/Perfil'
+//import Home from "./src/Pages/Home"
+import { lazy, Suspense } from "react";
 
 
 
-
+const Perfil = lazy(() => import('./src/Pages/Perfil'));
+const Home = lazy(() => import('./src/Pages/Home'));
 const Rotas = () => (
-
+    <Suspense fallback={'Loading'}>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/perfil/:id" element={<Perfil />}/>
       </Routes>
+    </Suspense>
 )
 
 
